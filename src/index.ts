a7eaa87ac1ad2e1de7cd78c295e6d5365ff1fc06
hiro-su <h.sugipon@gmail.com>
2020-08-28 12:11:56 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import Stripe from 'stripe';
 import { exit } from 'process';
 import moment from 'moment';
+import crypto from 'crypto';
 
+const userName = crypto.randomBytes(16).toString('base64').substring(0, 16);
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2020-03-02' });
 
 (async () => {
   const product = await stripe.products.create({
-    name: 'サブスクリプションテスト価格',
+    name: `サブスクリプションテスト価格-${userName}`,
   });
 
   const price = await stripe.prices.create({
@@ -19,9 +21,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion:
   const billing_cycle_anchor = moment().add({ months: 1 }).startOf('month').unix();
 
   const customer = await stripe.customers.create({
-    name: 'テスト 太郎',
-    email: 'user@example.com',
-    phone: '0000000000',
+    name: userName,
   });
 
   const card = await stripe.customers.createSource(customer.id, { source: 'tok_visa' });
@@ -32,6 +32,32 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion:
     proration_behavior: 'none',
     billing_cycle_anchor,
   });
+
+  const updatePrice = await stripe.prices.create({
+    unit_amount: 11000,
+    currency: 'jpy',
+    product: product.id,
+    recurring: { interval: 'month' },
+  });
+
+  await stripe.subscriptions.update(subscription.id, {
+    proration_behavior: 'none',
+    cancel_at_period_end: true,
+  });
+
+  await stripe.subscriptionSchedules.create({
+    from_subscription: subscription.id,
+  });
+
+  const start_date = moment().add({ months: 2 }).startOf('month').unix();
+  const suchedule = await stripe.subscriptionSchedules.create({
+    customer: customer.id,
+    start_date,
+    end_behavior: 'release',
+    phases: [{ plans: [{ price: updatePrice.id }] }],
+  });
+
+  console.log(suchedule);
 
   console.log('完了');
   exit(0);
