a2678a6fc0ebd29ff97e4c4358cd2ef706754907
hiro-su <h.sugipon@gmail.com>
2020-08-28 12:16:52 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import Stripe from 'stripe';
 import { exit } from 'process';
 import moment from 'moment';
 import crypto from 'crypto';
+import os from 'os';
 
 const userName = crypto.randomBytes(16).toString('base64').substring(0, 16);
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: '2020-03-02' });
 
 (async () => {
   const product = await stripe.products.create({
-    name: `サブスクリプションテスト価格-${userName}`,
+    name: `サブスクリプションテスト価格-${userName}-${os.hostname()}`,
   });
 
   const price = await stripe.prices.create({
@@ -21,7 +22,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion:
   const billing_cycle_anchor = moment().add({ months: 1 }).startOf('month').unix();
 
   const customer = await stripe.customers.create({
-    name: userName,
+    name: `${userName}-${os.hostname()}`,
   });
 
   const card = await stripe.customers.createSource(customer.id, { source: 'tok_visa' });
